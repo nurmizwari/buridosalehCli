798fe58624e8eb0c3edd6fa45a46a8d7212a506c
nur mizwari <nurmizwari04@gmail.com>
2023-07-15 15:49:34 +0700

    add push notification with image

--- App.tsx
+++ App.tsx
@@ -4,6 +4,8 @@ import {NavigationContainer} from '@react-navigation/native';
 import React, {useEffect} from 'react';
 import {BackHandler, Alert} from 'react-native';
 import MyTabs from './navigation';
+import messaging from '@react-native-firebase/messaging';
+import {PermissionsAndroid} from 'react-native';
 
 export default function App() {
   //! backhandler
@@ -27,6 +29,84 @@ export default function App() {
 
     return () => backHandler.remove();
   }, []);
+
+  async function requestUserPermission() {
+    const authStatus = await messaging().requestPermission();
+    const enabled =
+      authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+      authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+
+    if (enabled) {
+      console.log('Authorization status:', authStatus);
+    }
+  }
+
+  async function getTokenDevice() {
+    await messaging().registerDeviceForRemoteMessages();
+    const token = await messaging().getToken();
+    console.log(token);
+  }
+  function getUserPermissions() {
+    PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS,
+    );
+  }
+
+  useEffect(() => {
+    getUserPermissions();
+    requestUserPermission();
+    getTokenDevice();
+    const unsubscribe = messaging().onMessage(async remoteMessage => {
+      Alert.alert(
+        JSON.stringify(remoteMessage.notification?.title),
+        remoteMessage.data?.title,
+        // remoteMessage.notification?.body,
+      );
+    });
+
+    return unsubscribe;
+  }, []);
+
+  async function sendMessage() {
+    const FIREBASE_API_KEY =
+      'key=AAAAt0PqXzk:APA91bHNw6SPNuaocnfFdqKYuusdx8XrCxXlVxHjHmC1_JpjplzneucO5c8aKOHz4pTZ4sAmWQBC-WLdmMKNcwps8WY9YnDTiSeDydZ8wl_VrKxky79nteh3vMm6YPIjUyFlERj-oOBf';
+    const message = {
+      registration_ids: [
+        'cXLpt9oeQpSREGrNwqSFgw:APA91bGCtbxbWolcUSTkuw8jik3BBKpYjzndAzcPTGt-NixTWiYpiNeqXoFvGvOmtWUszfw9RAvI9289hrm5qO-rH_BnnjaOiANINd7GmAkx2Mh69VKHR7Tv9Mjb7RihNhnvUekkJoW7',
+        'xxxxx....',
+      ],
+      notification: {
+        title: 'india vs south africa test',
+        body: 'IND chose to bat',
+        vibrate: 1,
+        sound: 1,
+        show_in_foreground: true,
+        priority: 'high',
+        content_available: true,
+        image:
+          'https://hips.hearstapps.com/hmg-prod/images/2024-lamborghini-revuelto-127-641a1d518802b.jpg?crop=0.813xw:0.721xh;0.0994xw,0.128xh&resize=1200:*',
+      },
+      data: {
+        title: 'india vs south africa test',
+        body: 'IND chose to bat',
+        score: 50,
+        wicket: 1,
+      },
+    };
+
+    let headers = new Headers({
+      'Content-Type': 'application/json',
+      Authorization: 'key=' + FIREBASE_API_KEY,
+    });
+
+    let response = await fetch('https://fcm.googleapis.com/fcm/send', {
+      method: 'POST',
+      headers,
+      body: JSON.stringify(message),
+    });
+    response = await response.json();
+    console.log(response);
+  }
 
   return (
     <NavigationContainer>
--- screens/Login.js
+++ screens/Login.js
@@ -16,23 +16,66 @@ export default function Login() {
   const [name, setName] = useState('');
   const [password, setPassword] = useState('');
 
+  //   POST https://fcm.googleapis.com/v1/projects/myproject-b5ae1/messages:send HTTP/1.1
+
+  // Content-Type: application/json
+  // Authorization: Bearer ya29.ElqKBGN2Ri_Uz...HnS_uNreA
+
+  // {
+  //    "message":{
+  //       "token":"bk3RNwTe3H0:CI2k_HHwgIpoDKCIZvvDMExUdFQ3P1...",
+  //       "data":{},
+  //       "notification":{
+  //         "body":"This is an FCM notification message!",
+  //         "title":"FCM Message"
+  //       }
+  //    }
+  // }
+
   const handleLogin = async () => {
     // Kode untuk melakukan proses login, misalnya dengan mengirim data ke server
     console.log('name:', name);
     console.log('Password:', password);
-    const {data} = await axios.post(
-      'http://10.0.2.2:3000/login', // harus pake ini gantinya localhost biar konek ke server
-      {
-        Name: name,
-        Password: password,
+
+    const FIREBASE_API_KEY =
+      'AAAAt0PqXzk:APA91bHNw6SPNuaocnfFdqKYuusdx8XrCxXlVxHjHmC1_JpjplzneucO5c8aKOHz4pTZ4sAmWQBC-WLdmMKNcwps8WY9YnDTiSeDydZ8wl_VrKxky79nteh3vMm6YPIjUyFlERj-oOBf';
+    const message = {
+      registration_ids: [
+        'cXLpt9oeQpSREGrNwqSFgw:APA91bGCtbxbWolcUSTkuw8jik3BBKpYjzndAzcPTGt-NixTWiYpiNeqXoFvGvOmtWUszfw9RAvI9289hrm5qO-rH_BnnjaOiANINd7GmAkx2Mh69VKHR7Tv9Mjb7RihNhnvUekkJoW7',
+      ],
+      notification: {
+        title: 'india vs south africa test',
+        body: 'IND chose to bat',
+        vibrate: 1,
+        sound: 1,
+        show_in_foreground: true,
+        priority: 'high',
+        content_available: true,
+        image:
+          'https://hips.hearstapps.com/hmg-prod/images/2024-lamborghini-revuelto-127-641a1d518802b.jpg?crop=0.813xw:0.721xh;0.0994xw,0.128xh&resize=1200:*',
       },
-      // {
-      //   headers: {
-      //     'Content-Type': 'application/x-www-form-urlencoded',
-      //   },
-      // },
-    );
-    console.log(data, 'data');
+      data: {
+        title: 'india vs south africa test',
+        body: 'IND chose to bat',
+        score: 50,
+        wicket: 1,
+      },
+    };
+
+    let headers = new Headers({
+      'Content-Type': 'application/json',
+      Authorization: 'key=' + FIREBASE_API_KEY,
+    });
+
+    let response = await fetch('https://fcm.googleapis.com/fcm/send', {
+      method: 'POST',
+      headers,
+      body: JSON.stringify(message),
+    });
+    response = await response.json();
+    console.log(response);
+
+    // console.log(data, 'data');
   };
   async function getUser(name, password) {
     console.log('masuk lofin');
